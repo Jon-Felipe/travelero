3be599366cd6a155d12d167df5cc08cd9fd0ce91
Jon-Felipe <rodriguesjf7@gmail.com>
2024-04-12 17:31:46 +0100

    replaced text with icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { BsBasket, BsPerson } from 'react-icons/bs';
+
 function Navbar() {
   return (
     <header className='h-20 flex items-center justify-between px-8'>
@@ -7,14 +9,18 @@ function Navbar() {
       <div className='md:hidden'>
         <p className='text-4xl'>x</p>
       </div>
-      <nav className='hidden md:flex md:gap-x-4'>
+      <nav className='hidden md:flex md:gap-x-5'>
         <a href='#'>Home</a>
         <a href='#'>About</a>
         <a href='#'>Contact</a>
       </nav>
-      <div className='hidden md:flex md:gap-4'>
-        <p>cart</p>
-        <p>login</p>
+      <div className='hidden md:flex md:gap-5'>
+        <div className='border rounded-full shadow p-3'>
+          <BsBasket className='w-6 h-6' />
+        </div>
+        <div className='border rounded-full shadow p-3'>
+          <BsPerson className='w-6 h-6' />
+        </div>
       </div>
     </header>
   );

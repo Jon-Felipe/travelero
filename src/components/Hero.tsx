e08c1485ac4d28696cdcc3dd1a3dc70842844f7b
Jon-Felipe <rodriguesjf7@gmail.com>
2024-04-13 00:47:52 +0100

    hero form location icon and styling updates

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { BsGeoAlt } from 'react-icons/bs';
 
 type FormDataType = {
   location: string;
@@ -36,9 +37,10 @@ function Hero() {
           Find what makes you happy anytime, anywhere
         </p>
         <form className='bg-white border rounded-2xl lg:rounded-full mt-6 w-full max-w-[850px] mx-auto overflow-y-hidden lg:grid lg:grid-cols-[1fr_2fr_150px] lg:px-8 lg:py-4'>
-          <div className='flex items-center'>
+          <div className='flex items-center gap-x-4 px-6 lg:px-0 py-4 lg:py-0 border-b lg:border-b-0'>
+            <BsGeoAlt className='w-6 h-6' />
             {/* location */}
-            <div className='px-6 lg:px-0 py-4 lg:py-0 border-b lg:border-b-0 w-full'>
+            <div className='w-full'>
               <label htmlFor='location' className='block font-medium'>
                 Location
               </label>
@@ -49,7 +51,7 @@ function Hero() {
                 value={formData.location}
                 onChange={handleOnChange}
                 placeholder='Where are you going?'
-                className='text-sm text-slate-400 w-full focus:outline-none focus:'
+                className='text-sm text-slate-400 w-full focus:outline-none'
               />
             </div>
           </div>
@@ -85,7 +87,7 @@ function Hero() {
           </div>
           <button
             type='submit'
-            className='bg-blue-600 text-white font-bold text-base w-full py-4 lg:rounded-full'
+            className='bg-blue-600 text-white font-bold text-base w-full py-4 lg:py-0 lg:rounded-full'
             onClick={onFormSubmit}
           >
             Search

aaf7d243664416fe9eb410b449a99d02bbc29e3a
Jon-Felipe <rodriguesjf7@gmail.com>
2024-04-12 19:00:07 +0100

    nav toggle button different icon display on click

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,22 @@
-import { BsBasket, BsPerson } from 'react-icons/bs';
+import { useState } from 'react';
+import { BsBasket, BsPerson, BsX, BsList } from 'react-icons/bs';
 
 function Navbar() {
+  const [showNavLinks, setShowNavLinks] = useState<boolean>(false);
+
   return (
     <header className='h-20 flex items-center justify-between px-8'>
       <div>
         <h1 className='text-4xl'>Travelero</h1>
       </div>
       <div className='md:hidden'>
-        <p className='text-4xl'>x</p>
+        <button type='button' onClick={() => setShowNavLinks(!showNavLinks)}>
+          {showNavLinks ? (
+            <BsList className='w-8 h-8' />
+          ) : (
+            <BsX className='w-10 h-10' />
+          )}
+        </button>
       </div>
       <nav className='hidden md:flex md:gap-x-5'>
         <a href='#'>Home</a>

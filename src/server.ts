01bd56d4f2cd83abc9d9b711be9f8ac36628c038
Jon-Felipe <rodriguesjf7@gmail.com>
2024-04-14 17:51:05 +0100

    installed and configured typescript for backend

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express, { Express, Request, Response } from 'express';
+const app: Express = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+const port = 5100;
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));

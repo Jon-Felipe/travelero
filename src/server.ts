5b739c814b9066ad8b1ba9241392af91e51a2439
Jon-Felipe <rodriguesjf7@gmail.com>
2024-04-14 18:29:36 +0100

    get all tours and get single tours initial route

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,14 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
+app.get('/tours', (req: Request, res: Response) => {
+  res.send('get all tours');
+});
+
+app.get('/tours/:id', (req: Request, res: Response) => {
+  res.send('get signle tour');
+});
+
 const port = 5100;
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));

299d416f92dc97d7ab9f153ae5f3a511e6ce6ccf
Jon-Felipe <rodriguesjf7@gmail.com>
2024-04-15 21:38:18 +0100

    hashing user password with bcrypt before saving to db

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,17 +1,26 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 interface IUser {
-  firstName: String;
-  lastName: String;
-  email: String;
-  password: String;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
 }
 
-const userSchema = new mongoose.Schema<IUser>({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true, min: 6 },
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true, min: 6 },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 export default mongoose.model<IUser>('User', userSchema);

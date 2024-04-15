11977315c1f6b05e479008b3134121fcd6529736
Jon-Felipe <rodriguesjf7@gmail.com>
2024-04-15 19:31:13 +0100

    create and inserting user into db

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,8 +1,16 @@
 import express, { Request, Response } from 'express';
+import User from '../models/UserModel';
+
 const router = express.Router();
 
-router.post('/register', (req: Request, res: Response) => {
-  res.send('register user');
+router.post('/register', async (req: Request, res: Response) => {
+  const { firstName, lastName, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    throw new Error('email already exists');
+  }
+  const user = await User.create({ firstName, lastName, email, password });
+  res.status(200).json({ user });
 });
 
 router.post('/login', (req: Request, res: Response) => {

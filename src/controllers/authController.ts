51cd2bdd0b8677889ecec70b946587d4cf8d67e8
Jon-Felipe <rodriguesjf7@gmail.com>
2024-04-18 10:13:07 +0100

    separated auth router functionality into it's own controller function handler

---
+++ src/controllers/authController.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from 'express';
+import User from '../models/UserModel';
+
+// @desc    Register new user
+// @route   POST /api/v1/auth/register
+// @access  Public
+async function registerUser(req: Request, res: Response) {
+  const { firstName, lastName, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    throw new Error('email already exists');
+  }
+  await User.create({ firstName, lastName, email, password });
+  res.status(200).json({ msg: 'user created' });
+}
+
+// @desc    Login user
+// @route   POST /api/v1/auth/login
+// @access  Public
+async function loginUser(req: Request, res: Response) {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (user && (await user.comparePasswords(password))) {
+    res.status(200).json({ msg: 'user logged in' });
+  } else {
+    res.status(401);
+    throw new Error('Invalid credentials');
+  }
+}
+
+// @desc    Logout user
+// @route   GET /api/v1/auth/logout
+// @access  Public
+async function logoutUser(req: Request, res: Response) {
+  res.send('logout user');
+}
+
+export { registerUser, loginUser, logoutUser };
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,38 +1,15 @@
-import express, { Request, Response } from 'express';
-import User from '../models/UserModel';
+import express from 'express';
+
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from '../controllers/authController';
 
 const router = express.Router();
 
-// @desc    Register new user
-// @route   POST /api/v1/auth/register
-// @access  Public
-router.post('/register', async (req: Request, res: Response) => {
-  const { firstName, lastName, email, password } = req.body;
-  const userExists = await User.findOne({ email });
-  if (userExists) {
-    throw new Error('email already exists');
-  }
-  await User.create({ firstName, lastName, email, password });
-  res.status(200).json({ msg: 'user created' });
-});
-
-// @desc    Login user
-// @route   POST /api/v1/auth/login
-// @access  Public
-router.post('/login', async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-
-  const user = await User.findOne({ email });
-  if (user && (await user.comparePasswords(password))) {
-    res.status(200).json({ msg: 'user logged in' });
-  } else {
-    res.status(401);
-    throw new Error('Invalid credentials');
-  }
-});
-
-router.get('/logout', (req: Request, res: Response) => {
-  res.send('logout user');
-});
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/logout', logoutUser);
 
 export default router;

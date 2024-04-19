201df49d2cf9381c4ba1a25a35b93a443efc407e
Jon-Felipe <rodriguesjf7@gmail.com>
2024-04-19 10:52:43 +0100

    separated jwt token create method into separate file

---
+++ src/utils/tokenUtils.ts
@@ -0,0 +1,9 @@
+import jwt from 'jsonwebtoken';
+import { Types } from 'mongoose';
+
+export function createJWT(payload: { userId: Types.ObjectId }) {
+  const token = jwt.sign(payload, process.env.JWT_SECRET as string, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+  return token;
+}

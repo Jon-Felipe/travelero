5eef8c30afa93b201fd32920f3b6f3feced63e8f
Jon-Felipe <rodriguesjf7@gmail.com>
2024-04-21 11:53:44 +0100

    display profile icon if user value is in store

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 import { BsBasket, BsPerson, BsX, BsList } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
+import { useAppSelector } from '../hooks/hooks';
 
 function Navbar() {
   const [showNavLinks, setShowNavLinks] = useState<boolean>(false);
+
+  const { user } = useAppSelector((store) => store.auth);
 
   return (
     <header className='h-20 flex items-center justify-between px-8 border-b border-b-slate-300'>
@@ -31,12 +34,15 @@ function Navbar() {
         </a>
       </nav>
       <div className='hidden md:flex md:items-center md:gap-6'>
-        <div>
-          <Link to='/login'>Login/Register</Link>
-        </div>
-        {/* <div className='border rounded-full shadow p-3'>
-          <BsPerson className='w-5 h-5' />
-        </div> */}
+        {user.firstName ? (
+          <div className='border rounded-full shadow p-3'>
+            <BsPerson className='w-5 h-5' />
+          </div>
+        ) : (
+          <div>
+            <Link to='/login'>Login/Register</Link>
+          </div>
+        )}
         <div className='border rounded-full shadow p-3'>
           <BsBasket className='w-5 h-5' />
         </div>

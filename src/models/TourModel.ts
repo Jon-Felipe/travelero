ac854d3093863c74bc007e7efadebe71e109c3d9
Jon-Felipe <rodriguesjf7@gmail.com>
2024-04-25 20:37:20 +0100

    defined and associated tour model type

--- src/models/TourModel.ts
+++ src/models/TourModel.ts
@@ -1,18 +1,28 @@
 import mongoose from 'mongoose';
 
-const tourSchema = new mongoose.Schema(
+interface ITour {
+  title: string;
+  image: string;
+  country: string;
+  countryCode: string;
+  rating: number;
+  price: number;
+  duration: number;
+}
+
+const tourSchema = new mongoose.Schema<ITour>(
   {
     title: { type: String, required: true },
     image: { type: String, required: true },
     country: { type: String, required: true },
     countryCode: { type: String, required: true },
     rating: { type: Number, default: 0 },
-    price: { type: String, required: true },
+    price: { type: Number, required: true },
     duration: { type: Number, required: true },
   },
   { timestamps: true }
 );
 
-const Tour = mongoose.model('Tour', tourSchema);
+const Tour = mongoose.model<ITour>('Tour', tourSchema);
 
 export default Tour;

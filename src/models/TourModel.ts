125430b8fff408441ee4ff284501b1c9cfa37400
Jon-Felipe <rodriguesjf7@gmail.com>
2024-04-25 20:59:24 +0100

    added city to tour model and type

--- src/models/TourModel.ts
+++ src/models/TourModel.ts
@@ -6,6 +6,7 @@ interface ITour {
   country: string;
   countryISO: string;
   countryCode: string;
+  city: string;
   rating: number;
   price: number;
   duration: number;
@@ -18,6 +19,7 @@ const tourSchema = new mongoose.Schema<ITour>(
     country: { type: String, required: true },
     countryISO: { type: String, required: true },
     countryCode: { type: String, required: true },
+    city: { type: String, required: true },
     rating: { type: Number, default: 0 },
     price: { type: Number, required: true },
     duration: { type: Number, required: true },

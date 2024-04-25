236c8bbc25376e61b7af956326814f085864393c
Jon-Felipe <rodriguesjf7@gmail.com>
2024-04-25 20:58:20 +0100

    added country code to tour model and type

--- src/models/TourModel.ts
+++ src/models/TourModel.ts
@@ -4,6 +4,7 @@ interface ITour {
   title: string;
   image: string;
   country: string;
+  countryISO: string;
   countryCode: string;
   rating: number;
   price: number;
@@ -15,6 +16,7 @@ const tourSchema = new mongoose.Schema<ITour>(
     title: { type: String, required: true },
     image: { type: String, required: true },
     country: { type: String, required: true },
+    countryISO: { type: String, required: true },
     countryCode: { type: String, required: true },
     rating: { type: Number, default: 0 },
     price: { type: Number, required: true },

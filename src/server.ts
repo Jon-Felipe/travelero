1e7b30772c5c8b42807ae46b0db830911515eef5
Jon-Felipe <rodriguesjf7@gmail.com>
2024-05-01 17:52:53 +0100

    not found and error handler middleware

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import 'express-async-errors';
 import 'dotenv/config';
-import express, { Express, Request, Response } from 'express';
+import express, {
+  Express,
+  NextFunction,
+  Request,
+  Response,
+  ErrorRequestHandler,
+} from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 
@@ -22,6 +28,22 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
+
+app.use('*', (req: Request, res: Response) => {
+  res.status(404).json({ msg: 'not found' });
+});
+
+app.use(
+  (
+    err: ErrorRequestHandler,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    console.log(err);
+    res.status(500).json({ msg: 'something went wrong' });
+  }
+);
 
 const port = process.env.PORT || 5100;
 

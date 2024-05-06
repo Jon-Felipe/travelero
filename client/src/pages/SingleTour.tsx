9d5623c2c107c61349d572ecf0dcd05035c8beec
Jon-Felipe <rodriguesjf7@gmail.com>
2024-05-06 20:42:45 +0100

    html semantics updates

--- client/src/pages/SingleTour.tsx
+++ client/src/pages/SingleTour.tsx
@@ -52,7 +52,7 @@ function SingleTour({}: Props) {
       {/* tour body */}
       <section>
         {/* tour features */}
-        <article className='grid grid-cols-2 md:grid-cols-4 grid-rows-2 md:grid-rows-1 gap-4 md:gap-y-0 mt-10'>
+        <section className='grid grid-cols-2 md:grid-cols-4 grid-rows-2 md:grid-rows-1 gap-4 md:gap-y-0 mt-10'>
           {/* duration */}
           <TourFeature
             icon={<BsClock className='w-5 h-5' />}
@@ -77,7 +77,7 @@ function SingleTour({}: Props) {
             title='Languages'
             text={data?.tour.languages.join(', ')!}
           />
-        </article>
+        </section>
         <hr className='my-10' />
         {/* tour description */}
         <article className='mb-5'>
@@ -95,7 +95,7 @@ function SingleTour({}: Props) {
         <hr className='my-10' />
         {/* included/excluded */}
         {data?.tour.included && data?.tour.excluded && (
-          <article>
+          <section>
             <div className='block md:flex md:items-center md:gap-x-16'>
               <TourFeatureList
                 title='Included/Excluded'
@@ -107,7 +107,7 @@ function SingleTour({}: Props) {
                 icon={<BsFillXCircleFill className='w-5 h-5 text-red-400' />}
               />
             </div>
-          </article>
+          </section>
         )}
       </section>
     </div>
@@ -124,7 +124,7 @@ type FeatureProps = {
 
 function TourFeature({ icon, title, text }: FeatureProps) {
   return (
-    <div className='flex items-center'>
+    <article className='flex items-center'>
       <div className='flex items-center justify-center border border-slate-200 rounded-xl p-3 w-12 h-12 mr-4'>
         {icon}
       </div>
@@ -132,7 +132,7 @@ function TourFeature({ icon, title, text }: FeatureProps) {
         <h4 className='text-base font-bold capitalize'>{title}</h4>
         <p className='text-sm font-normal text-slate-500 capitalize'>{text}</p>
       </div>
-    </div>
+    </article>
   );
 }
 

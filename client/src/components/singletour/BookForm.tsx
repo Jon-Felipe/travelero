a1e1d86b74d3d767fa4e63d866e45cd015bc8ca8
Jon-Felipe <rodriguesjf7@gmail.com>
2024-05-07 21:10:08 +0100

    single tour book form inputs

--- client/src/components/singletour/BookForm.tsx
+++ client/src/components/singletour/BookForm.tsx
@@ -1,7 +1,85 @@
-type Props = {};
+import { BsPlusCircle, BsDashCircle } from 'react-icons/bs';
 
-function BookForm({}: Props) {
-  return <div>BookForm</div>;
+function BookForm() {
+  return (
+    <form>
+      <div className='border rounded-xl'>
+        <div className='p-4'>
+          <label htmlFor='date' className='block font-medium'>
+            Date
+          </label>
+          <input type='date' className='text-slate-400' />
+        </div>
+        <hr />
+        {/* adults input */}
+        <div className='p-4 flex items-center justify-between'>
+          <div>
+            <label htmlFor='date' className='block font-medium'>
+              Adults
+            </label>
+            <p>Over 18+</p>
+          </div>
+          <div className='flex items-center justify-between gap-x-4'>
+            <button>
+              <BsDashCircle className='w-6 h-6' />
+            </button>
+            <p>0</p>
+            <button>
+              <BsPlusCircle className='w-6 h-6' />
+            </button>
+          </div>
+        </div>
+        <hr />
+        {/* children input */}
+        <div className='p-4 flex items-center justify-between'>
+          <div>
+            <label htmlFor='date' className='block font-medium'>
+              Children
+            </label>
+            <p>Under 12</p>
+          </div>
+          <div className='flex items-center justify-between gap-x-4'>
+            <button>
+              <BsDashCircle className='w-6 h-6' />
+            </button>
+            <p>0</p>
+            <button>
+              <BsPlusCircle className='w-6 h-6' />
+            </button>
+          </div>
+        </div>
+        <hr />
+        {/* infant input */}
+        <div className='p-4 flex items-center justify-between'>
+          <div>
+            <label htmlFor='date' className='block font-medium'>
+              Infant
+            </label>
+            <p>Under 3</p>
+          </div>
+          <div className='flex items-center justify-between gap-x-4'>
+            <button>
+              <BsDashCircle className='w-6 h-6' />
+            </button>
+            <p>0</p>
+            <button>
+              <BsPlusCircle className='w-6 h-6' />
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* total */}
+      <div className='p-4 flex items-center justify-between'>
+        <h5 className='text-lg font-bold'>Total</h5>
+        <p className='text-lg font-bold'>$250.00</p>
+      </div>
+      <div className='w-11/12 mx-auto'>
+        <button className='w-full bg-blue-500 text-white py-3 text-base font-medium rounded-full'>
+          Book now
+        </button>
+      </div>
+    </form>
+  );
 }
 
 export default BookForm;

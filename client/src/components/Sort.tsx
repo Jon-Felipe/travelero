ffbef300e69cc3472930d141a701319b218a9a76
Jon-Felipe <rodriguesjf7@gmail.com>
2024-05-13 16:02:20 +0100

    sort options value changes

--- client/src/components/Sort.tsx
+++ client/src/components/Sort.tsx
@@ -16,11 +16,11 @@ function Sort({}: Props) {
         <option value='default' disabled>
           Sort
         </option>
+        <option value='a-z'>Ascending</option>
+        <option value='z-a'>Descending</option>
         <option value='newest'>New Tours</option>
         <option value='price-highest'>Price Highest</option>
         <option value='price-lowest'>Price Lowest</option>
-        <option value='a-z'>A-Z</option>
-        <option value='z-a'>Z-A</option>
       </select>
     </form>
   );

2679067cdf8a12979f427ae6bafda609f4463510
Jon-Felipe <rodriguesjf7@gmail.com>
2024-05-26 14:36:48 +0100

    changed button to link button

--- client/src/components/TourListView.tsx
+++ client/src/components/TourListView.tsx
@@ -3,6 +3,7 @@ import { FaShoePrints } from 'react-icons/fa';
 
 // extras
 import { Tour } from '../utils/types';
+import { Link } from 'react-router-dom';
 
 type Props = {
   tours: Tour[] | undefined;
@@ -56,9 +57,12 @@ function TourListView({ tours }: Props) {
                       ${tour.price}
                     </span>
                   </p>
-                  <button className='bg-blue-500 text-white px-4 py-2 rounded-full'>
+                  <Link
+                    to={`/tours/${tour._id}`}
+                    className='bg-blue-500 text-white px-4 py-2 rounded-full'
+                  >
                     View details
-                  </button>
+                  </Link>
                 </div>
               </div>
             </div>

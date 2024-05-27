c5f64b6f35e247b557ab234dbba90f3cdeaaae83
Jon-Felipe <rodriguesjf7@gmail.com>
2024-05-27 08:31:12 +0100

    inquiry form form start and handle change functionality

--- client/src/components/singletour/InquiryForm.tsx
+++ client/src/components/singletour/InquiryForm.tsx
@@ -1,34 +1,70 @@
+import { useState } from 'react';
+
 // components
 import FormRow from '../FormRow';
 
+type FormData = {
+  name: string;
+  email: string;
+  phone: string;
+  note: string;
+};
+
+const initialFormData: FormData = {
+  name: '',
+  email: '',
+  phone: '',
+  note: '',
+};
+
 function InquiryForm() {
+  const [formData, setFormData] = useState<FormData>(initialFormData);
+
+  function handleOnChange(
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setFormData((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  }
+
   return (
-    <form>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log('form submitted');
+      }}
+    >
       <div className='space-y-5'>
         <FormRow
           type='text'
           name='name'
-          value=''
-          onChange={() => console.log('name')}
+          value={formData.name}
+          onChange={handleOnChange}
           placeholder='Name*'
         />
         <FormRow
           type='email'
           name='email'
-          value=''
-          onChange={() => console.log('email')}
+          value={formData.email}
+          onChange={handleOnChange}
           placeholder='Email*'
         />
         <FormRow
-          type='number'
+          type='string'
           name='phone'
-          value=''
-          onChange={() => console.log('phone')}
+          value={formData.phone}
+          onChange={handleOnChange}
           placeholder='Phone*'
         />
         <textarea
           name='note'
           id='note'
+          value={formData.note}
+          onChange={handleOnChange}
           placeholder='Note*'
           rows={3}
           className='border-2 px-4 py-2 rounded-md w-full'

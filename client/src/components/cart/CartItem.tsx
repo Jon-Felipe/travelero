b99b4767f5114fb861559115252fbb869341b1cd
Jon-Felipe <rodriguesjf7@gmail.com>
2024-06-04 14:26:32 +0100

    displaying user selected cart items

---
+++ client/src/components/cart/CartItem.tsx
@@ -0,0 +1,61 @@
+import { BsClock, BsPeople, BsPencil, BsTrash } from 'react-icons/bs';
+
+// components
+import Rating from '../Rating';
+
+// extras
+import { Cart } from '../../utils/types';
+
+type Props = {
+  cartItem: Partial<Cart>;
+};
+
+function CartItem({ cartItem: { tour, date, guests } }: Props) {
+  return (
+    <article className='border rounded-xl p-4 mb-4'>
+      <div className='flex gap-x-4'>
+        <img
+          src={tour?.image}
+          alt={tour?.title}
+          className='w-24 h-24 object-cover rounded-xl'
+        />
+        {/* cart item info */}
+        <div>
+          <h6 className='font-semibold'>{tour?.title}</h6>
+          {/* cart item details */}
+          <div className='flex items-center gap-x-2 mb-2'>
+            <Rating value={tour?.rating!} />
+            <p className='text-sm'>{tour?.rating}</p>
+            <p className='text-sm text-slate-500'>(69)</p>
+          </div>
+          {/* cart item user info */}
+          <div className='space-y-1'>
+            <div className='flex items-center gap-x-2'>
+              <BsClock />
+              <p>{date}</p>
+            </div>
+            <div className='flex items-center gap-x-2'>
+              <BsPeople />
+              <p>
+                {guests?.adults ? guests.adults + ' Adults' : ''}
+                {guests?.children ? ', ' + guests.children + ' Children' : ''}
+                {guests?.infants ? ', ' + guests.infants + ' Infants' : ''}
+              </p>
+            </div>
+          </div>
+          {/* cart item action buttons */}
+          <div className='flex items-center gap-x-2 mt-2'>
+            <button className='flex items-center gap-x-2 bg-slate-200 py-2 px-4 rounded-2xl text-sm font-semibold shadow-sm'>
+              <BsPencil /> Edit
+            </button>
+            <button className='p-3 bg-slate-200 rounded-full shadow-sm'>
+              <BsTrash className='w-4 h-4' />
+            </button>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default CartItem;
--- client/src/pages/Cart.tsx
+++ client/src/pages/Cart.tsx
@@ -1,7 +1,25 @@
+import { useAppSelector } from '../hooks/hooks';
+
+// components
+import CartItem from '../components/cart/CartItem';
+
 type Props = {};
 
 function Cart({}: Props) {
-  return <div>Cart</div>;
+  const { cart } = useAppSelector((store) => store.cart);
+
+  return (
+    <div className='max-w-screen-2xl mx-auto mt-8 p-6 grid lg:grid-cols-2 lg:gap-x-20'>
+      {/* cart */}
+      <section>
+        {cart.map((cartItem, index) => {
+          return <CartItem key={index} cartItem={cartItem} />;
+        })}
+      </section>
+      {/* cart summary */}
+      <section>cart summary</section>
+    </div>
+  );
 }
 
 export default Cart;

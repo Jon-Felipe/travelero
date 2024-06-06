4069a807d4563a968710711a6202e0e87dd9c0c3
Jon-Felipe <rodriguesjf7@gmail.com>
2024-06-06 14:57:25 +0100

    refactored navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,59 +1,48 @@
-import { useState } from 'react';
-import { BsBasket, BsPerson, BsX, BsList } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
-import { useAppSelector } from '../hooks/hooks';
+// import { useState } from 'react';
+import { BsPerson, BsCart2, BsHeart } from 'react-icons/bs';
+// import { Link } from 'react-router-dom';
+// import { useAppSelector } from '../hooks/hooks';
 
 function Navbar() {
-  const [showNavLinks, setShowNavLinks] = useState<boolean>(false);
+  // const [showNavLinks, setShowNavLinks] = useState<boolean>(false);
 
-  const { user } = useAppSelector((store) => store.user);
+  // const { user } = useAppSelector((store) => store.user);
 
   return (
-    <header className='h-20 flex items-center justify-between px-8 border-b border-b-slate-300'>
+    <header className='h-20 flex items-center justify-between px-8 max-w-screen-xl mx-auto'>
       <div>
-        <h1 className='text-4xl font-bold'>Travelero</h1>
+        <h1 className='text-2xl md:text-4xl font-bold'>Travelero</h1>
       </div>
-      <div className='md:hidden'>
-        <button type='button' onClick={() => setShowNavLinks(!showNavLinks)}>
-          {showNavLinks ? (
-            <BsList className='w-8 h-8' />
-          ) : (
-            <BsX className='w-10 h-10' />
-          )}
-        </button>
-      </div>
-      <nav className='hidden md:flex md:gap-x-10'>
-        <Link to='/' className='font-medium text-xl'>
-          Home
-        </Link>
-        <Link to='/tours' className='font-medium text-xl'>
-          Tours
-        </Link>
-        <Link to='/' className='font-medium text-xl'>
-          About
-        </Link>
-        <Link to='/' className='font-medium text-xl'>
-          Contact
-        </Link>
-      </nav>
-      <div className='hidden md:flex md:items-center md:gap-6'>
-        {user ? (
-          <div className='border rounded-full shadow p-3'>
-            <Link to='/profile'>
-              <BsPerson className='w-5 h-5' />
-            </Link>
-          </div>
-        ) : (
-          <div>
-            <Link to='/login'>Login/Register</Link>
-          </div>
-        )}
-        <div className='border rounded-full shadow p-3'>
-          <BsBasket className='w-5 h-5' />
-        </div>
+      <div className='flex items-center gap-x-6 md:gap-x-8'>
+        <NavbarLink
+          icon={<BsHeart className='md:w-5 md:h-5' />}
+          text='Wishlist'
+        />
+        <NavbarLink
+          icon={<BsCart2 className='md:w-5 md:h-5' />}
+          text='Wishlist'
+        />
+        <NavbarLink
+          icon={<BsPerson className='md:w-5 md:h-5' />}
+          text='Wishlist'
+        />
       </div>
     </header>
   );
 }
 
 export default Navbar;
+
+type NavbarLinkProp = {
+  icon: React.ReactElement;
+  text: string;
+};
+
+function NavbarLink({ icon, text }: NavbarLinkProp) {
+  return (
+    <div className='flex flex-col items-center'>
+      {icon}
+      <p className='text-sm md:text-base'>{text}</p>
+    </div>
+  );
+}

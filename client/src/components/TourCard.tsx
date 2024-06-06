671f7312179258ad2e7784d39f0e9045ee24bcc6
Jon-Felipe <rodriguesjf7@gmail.com>
2024-06-06 20:27:34 +0100

    tour card styling refactor

--- client/src/components/TourCard.tsx
+++ client/src/components/TourCard.tsx
@@ -1,4 +1,4 @@
-import { BsGeoAlt, BsClock, BsFillStarFill } from 'react-icons/bs';
+import { BsFillStarFill } from 'react-icons/bs';
 
 // extras
 import { Tour } from '../utils/types';
@@ -9,39 +9,32 @@ type Props = {
 
 function TourCard({ tour }: Props) {
   return (
-    <article className='rounded-2xl overflow-hidden bg-white shadow'>
+    <article className='grid grid-rows-[200px_250px] overflow-hidden rounded border'>
       <img
         src={tour.image}
         alt={tour.title}
-        className='w-full h-[300px] object-cover'
+        className='w-full h-full object-cover'
       />
-      <div className='p-5'>
-        <p className='flex items-start gap-x-2 text-sm font-normal text-gray-400'>
-          <span>
-            <BsGeoAlt />
-          </span>
-          {tour.country}, {tour.countryISO}
-        </p>
-        <h3 className='text-lg font-bold mt-2 mb-4 capitalize truncate'>
-          {tour.title}
-        </h3>
-        <div className='flex items-center gap-x-2 mb-4'>
-          <p className='flex items-start gap-x-1 font-bold text-base'>
-            <span>
-              <BsFillStarFill className='text-orange-500 w-4 h-4' />
-            </span>{' '}
-            {tour.rating}
+      <div className='flex flex-col justify-between py-2 px-4'>
+        <div>
+          <p className='text-base capitalize font-medium text-slate-600'>
+            {tour.tourType}
           </p>
-          <p className='font-normal text-sm text-gray-400'>(69 Review(s))</p>
+          <h3 className='text-base font-bold mb-4 capitalize mt-1'>
+            {tour.city}: {tour.title}
+          </h3>
         </div>
-        <hr />
-        <div className='flex items-center justify-between mt-5'>
-          <p className='text-base font-bold'>${tour?.price?.toFixed(2)}</p>
-          <p className='flex items-center gap-x-2 text-sm font-normal text-gray-400'>
-            <span>
-              <BsClock />
-            </span>
-            {tour.duration}
+        <div>
+          <div className='flex items-center gap-x-2 mb-1'>
+            <p className='flex items-center gap-x-1 font-medium text-sm'>
+              <BsFillStarFill className='text-yellow-400 w-4 h-4' />
+              {tour.rating}
+            </p>
+            <p className='font-normal text-xs text-gray-600'>(69 Review(s))</p>
+          </div>
+          <p className='text-sm font-medium'>
+            From ${tour.price?.toFixed(2)}{' '}
+            <span className='text-xs'>per person</span>
           </p>
         </div>
       </div>
